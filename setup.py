9299afa03fa7b54c64ccbc06defb28b1deca0164
Oz Akan <oz@akan.me>
2017-12-04 10:08:39 -0500

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-  name = 'rssfeeder',
-  packages = ['rssfeeder'],
-  version = '0.1',
-  description = 'A library to read RSS feeds and post to different destinations.',
-  author = 'Oz Akan',
-  author_email = 'oz@akan.me',
-  url = 'https://github.com/ozgurakan/rssfeeder',
-  keywords = ['rss', 'feed', 'aws', 'dynamodb'],
-  classifiers = [],
+    name='rssfeeder',
+    version='0.1.1',
+    description='A library to read RSS feeds and post to different destinations.',
+    author='Oz Akan',
+    author_email='oz@akan.me',
+    url='https://github.com/ozgurakan/rssfeeder',
+    keywords=['rss', 'feed', 'aws', 'dynamodb'],
+    classifiers=[],
+    packages=['rssfeeder'],
+    install_requires=['boto3', 'feedparser'],
+    python_requires='>=3',
 )
